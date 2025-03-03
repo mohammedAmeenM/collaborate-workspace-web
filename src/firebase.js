9eb433fdd6d9d1d64d7273d87cf2a1470076c195
amiiiiee <amiameen507@gmail.com>
2025-03-03 17:13:32 +0530

    edits

--- src/firebase.js
+++ src/firebase.js
@@ -1,16 +1,15 @@
-
 import { initializeApp } from "firebase/app";
-import { getFirestore, doc, setDoc, onSnapshot, updateDoc, collection, getDocs, getDoc, query, where,addDoc } from "firebase/firestore";
+import { getFirestore, doc, setDoc, onSnapshot, updateDoc, collection, getDocs, getDoc, query, where, addDoc } from "firebase/firestore";
 import { getAuth, onAuthStateChanged } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB70QF98gP83rnRNdDP_mN1H-NnoAhYi8Y",
-  authDomain: "collaborative-app-3a134.firebaseapp.com",
-  projectId: "collaborative-app-3a134",
-  storageBucket: "collaborative-app-3a134.firebasestorage.app",
-  messagingSenderId: "498705675091",
-  appId: "1:498705675091:web:405b186d21db2f8d2967a6",
-  measurementId: "G-LXEF90JS1C",
+    apiKey: "AIzaSyB70QF98gP83rnRNdDP_mN1H-NnoAhYi8Y",
+    authDomain: "collaborative-app-3a134.firebaseapp.com",
+    projectId: "collaborative-app-3a134",
+    storageBucket: "collaborative-app-3a134.firebasestorage.app",
+    messagingSenderId: "498705675091",
+    appId: "1:498705675091:web:405b186d21db2f8d2967a6",
+    measurementId: "G-LXEF90JS1C",
 };
 
 // Initialize Firebase
@@ -20,18 +19,18 @@ const auth = getAuth(app);
 
 
 
-export { 
-  db, 
-  doc, 
-  setDoc, 
-  onSnapshot, 
-  updateDoc, 
-  collection, 
-  getDocs,
-  getDoc,
-  query,
-  where,
-  auth,
-  addDoc,
-  onAuthStateChanged
+export {
+    db,
+    doc,
+    setDoc,
+    onSnapshot,
+    updateDoc,
+    collection,
+    getDocs,
+    getDoc,
+    query,
+    where,
+    auth,
+    addDoc,
+    onAuthStateChanged
 };
