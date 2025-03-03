34661642b217088b2112dbfbde28d2cb9573d15a
amiiiiee <amiameen507@gmail.com>
2025-03-03 17:08:29 +0530

    edits

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import Workspace from './pages/Workspace'
-import Login from './pages/Login'
-import Register from './pages/Register'
-import { AuthProvider } from './contexts/AuthContext';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import Workspace from "./pages/Workspace";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import { AuthProvider } from "./contexts/AuthContext";
 
 const App = () => {
   return (
@@ -14,7 +19,6 @@ const App = () => {
           <Route path="/workspace" element={<Workspace />} />
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Register />} />
-
         </Routes>
       </Router>
     </AuthProvider>
